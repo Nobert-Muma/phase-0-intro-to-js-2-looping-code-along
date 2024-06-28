1cbf88e948f65d2c601517e2635e9387a65b5281
Nobert-Muma <mumanobert17@gmail.com>
2024-06-28 23:48:00 +0300

    Working with loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let spy=[];
+function writeCards(names, evet) {
+    for (let name of names) {
+      spy.push(`Thank you, ${name}, for the wonderful ${evet} gift!`);
+    }
+    return spy;
+}
+
+
+function countDown() {
+    for (let i=10; i>-1; i--) {
+        console.log(i)
+    }
+}
+countDown();
